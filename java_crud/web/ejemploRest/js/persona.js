f2293bef2ea40d431b21aa740152476fc44a2410
maximilianou <maximilianou@thinkpad>
2015-10-13 17:26:59 -0300

    agregando mustachejs

--- java_crud/web/ejemploRest/js/persona.js
+++ java_crud/web/ejemploRest/js/persona.js
@@ -60,6 +60,11 @@ PERSONA.consultar = function(){
     xhr.onreadystatechange = function(){
         if( xhr.readyState === 4 && xhr.status === 200){
             document.querySelector('#panelResultados').innerHTML += xhr.responseText + '<br/>';
+            
+            var templatePersonas = document.querySelector("#templatePersonas").innerHTML;
+            var personas = {};
+            personas.listaPersonas = JSON.parse( xhr.responseText );
+            document.querySelector('#panelResultados').innerHTML += Mustache.render(  templatePersonas, personas);
         }
     };
     // objeto para enviar los parametros del formulario
