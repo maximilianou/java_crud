b8c449d29ed7cba9143ef14ad0de107a27dd6b4b
Maximiliano Usich <maximilianou@gmail.com>
2016-04-04 11:36:16 -0300

    base rest

--- java_crud/src/java/ejemploRest/PersonaServer.java
+++ java_crud/src/java/ejemploRest/PersonaServer.java
@@ -1,9 +1,7 @@
 package ejemploRest;
 
 import com.google.gson.Gson;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
