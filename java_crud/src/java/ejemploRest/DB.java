4383d2e57a608a2f9e0a6db535a6e8f891aa875a
educacion <educacion@localhost>
2017-09-05 19:24:01 +0000

    Conectando ala base, poniendo mal el usuario y password

--- java_crud/src/java/ejemploRest/DB.java
+++ java_crud/src/java/ejemploRest/DB.java
@@ -10,8 +10,8 @@ public class DB {
     private static DB INSTANCE = null;
     //private static String LABASE = "jdbc:hsqldb:file:"+System.getProperty("user.home")+"/personas.hsqldb";
     private static String LABASE = "jdbc:mysql://localhost/dbrest";
-    private static String LABASEUSUARIO = "root";  // "root";
-    private static String LABASECLAVE = "";    //"root";
+    private static String LABASEUSUARIO = "educacion";  // "root";
+    private static String LABASECLAVE = "educacion";    //"root";
     public static DB getInstance() throws ClassNotFoundException, IOException, SQLException {
         if (INSTANCE == null) {
             INSTANCE = new DB();
