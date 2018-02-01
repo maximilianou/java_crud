4f078b4a9a400c623fc2f5234a7d236b2b17ecb6
educacion <educacion@blanca>
2018-02-01 16:25:56 -0300

    ES7 async await delete

--- java_crud/web/ejemploRest/js/persona04.js
+++ java_crud/web/ejemploRest/js/persona04.js
@@ -1,12 +1,10 @@
 class Persona {
-
     static inicializar() {
         let elemInsertar = document.querySelector('#btnInsertar');
         elemInsertar.setAttribute('onclick', "Persona.insertar();");
         let elemConsultar = document.querySelector('#btnConsultar');
         elemConsultar.setAttribute('onclick', "Persona.consultar();");
     }
-
     static insertar() {
         let persona = {};
         persona.nombre = document.querySelector("#persona_nombre").value;
@@ -21,7 +19,6 @@ class Persona {
                     document.querySelector('#panelResultados').innerHTML = datotexto;
                 });
     }
-
     static actualizar(paramId) {
         let persona = {};
         persona.id = paramId;
@@ -37,23 +34,23 @@ class Persona {
                     document.querySelector('#panelResultados').innerHTML = datotexto;
                 });
     }
-
     static eliminar(paramId) {
-        let persona = {};
-        persona.id = paramId;
-        persona.nombre = document.querySelector("#persona_nombre_" + paramId).value;
-        persona.email = document.querySelector("#persona_email_" + paramId).value;
-        let personaStringJSON = JSON.stringify(persona);
-        fetch("../PersonaServer?&q=" + personaStringJSON,
-                {method: 'DELETE'})
-                .then(function (response) {
-                    return response.text();
-                })
-                .then(function (datotexto) {
-                    document.querySelector('#panelResultados').innerHTML = datotexto;
+        const traer = async() => {
+            let persona = {};
+            persona.id = paramId;
+            persona.nombre = document.querySelector("#persona_nombre_" + paramId).value;
+            persona.email = document.querySelector("#persona_email_" + paramId).value;
+            let personaStringJSON = JSON.stringify(persona);
+            let respuesta = await fetch("../PersonaServer?&q=" + personaStringJSON,
+                    {method: 'DELETE'});
+            let datotexto = JSON.parse(await respuesta.text());
+            document.querySelector('#panelResultados').innerHTML = datotexto;
+        }
+        traer()
+                .catch(ex => {
+                    document.querySelector("#panelMensajes").innerHTML = 'ERROR: ' + ex.message;
                 });
     }
-
     static consultar() {
         const traer = async() => {
             let respuesta = await fetch("../PersonaServer", {method: 'GET'});
